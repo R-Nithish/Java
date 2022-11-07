cdb6eb7b422487d11bdb6d658c97acf26bceeb30
Nithish <70053308+R-Nithish@users.noreply.github.com>
2022-11-07 08:19:27 +0530

    Create File.java

---
+++ File.java
@@ -0,0 +1,38 @@
+class Employee implements Serializable {
+ private static final long serialVersionUID = 1L;
+ private int id;
+ private String name;
+
+ public int getId() {
+  return id;
+ }
+
+ public void setId(int id) {
+  this.id = id;
+ }
+
+ public String getName() {
+  return name;
+ }
+
+ public void setName(String name) {
+  this.name = name;
+ }
+}
+
+public class ObjectOutputStreamExample {
+
+ public static void main(String[] args) {
+  final Employee employee = new Employee();
+  employee.setId(100);
+  employee.setName("ramesh");
+  try (final FileOutputStream fout = new FileOutputStream("employees.txt");
+    final ObjectOutputStream out = new ObjectOutputStream(fout)) {
+   out.writeObject(employee);
+   out.flush();
+   System.out.println("success");
+  } catch (IOException e) {
+   e.printStackTrace();
+  }
+ }
+}
